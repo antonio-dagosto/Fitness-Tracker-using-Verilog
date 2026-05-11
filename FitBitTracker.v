2d3167486af47e832a0db1156acb3bf93e6fb86e
Anthony D'Agosto <anthonydagosto@icloud.com>
2026-05-10 20:43:01 -0700

    Update FitBitTracker.v

--- FitBitTracker.v
+++ FitBitTracker.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/30/2024 11:42:00 AM
-// Design Name: 
-// Module Name: FitBitTracker
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module FitBitTracker(
 input pulse, clk, reset,
 output reg [13:0] step_count,

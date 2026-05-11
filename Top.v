bac3fa1f63c32ef4b50de7f3ef93dc181bfbdf2f
Anthony D'Agosto <anthonydagosto@icloud.com>
2026-05-10 20:42:48 -0700

    Update Top.v

--- Top.v
+++ Top.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/31/2024 04:16:45 PM
-// Design Name: 
-// Module Name: Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Top(
     input clk100Mhz,          // 100 MHz system clock
     input reset,              // System reset
